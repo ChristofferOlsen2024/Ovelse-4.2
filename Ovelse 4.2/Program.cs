e15a80340c9f02a15b0d2fbfa0842a062e0e6e03
Christoffer Olsen <chris70944@gmail.com>
2024-03-20 21:55:42 +0100

    Done

--- Ovelse 4.2/Program.cs
+++ Ovelse 4.2/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Skriv temperatur i dag.");
+
+int temp = Convert.ToInt32(Console.ReadLine());
+
+if (temp >= 20)
+{
+    Console.WriteLine("Det er dejligt varmt.");
+}
